76efdc35d5354c952fed741f43b1331e34c28f98
ciorb <ciorb@DESKTOP-QFS6OPT>
2024-01-25 11:59:17 +0200

    Add project files.

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
